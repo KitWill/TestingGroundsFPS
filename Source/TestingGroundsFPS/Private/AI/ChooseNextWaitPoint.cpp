c33b125f347e8fd31795ef595b8b2181a2e39f16
KitWillGuy <c743047798@gmail.com>
2018-12-25 13:52:10 +0800

    Create AI Behavior Tree Tasks

---
+++ Source/TestingGroundsFPS/Private/AI/ChooseNextWaitPoint.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/AI/ChooseNextWaitPoint.h"
+#include "BehaviorTree/BehaviorTreeTypes.h "
+
+
+
+
+EBTNodeResult::Type UChooseNextWaitPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::Succeeded;
+}  
