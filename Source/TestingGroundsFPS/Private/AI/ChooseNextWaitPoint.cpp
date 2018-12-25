0a21b1670f3d7e1cd158d186b31537d86f10b7bc
KitWillGuy <c743047798@gmail.com>
2018-12-25 14:23:00 +0800

    Read Blackboard Data In C++

--- Source/TestingGroundsFPS/Private/AI/ChooseNextWaitPoint.cpp
+++ Source/TestingGroundsFPS/Private/AI/ChooseNextWaitPoint.cpp
@@ -1,12 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Public/AI/ChooseNextWaitPoint.h"
-#include "BehaviorTree/BehaviorTreeTypes.h "
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 
 
 EBTNodeResult::Type UChooseNextWaitPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	auto Blackboard = OwnerComp.GetBlackboardComponent();
+
+	auto Index = Blackboard->GetValueAsInt(IndexKey.SelectedKeyName);
+
 	return EBTNodeResult::Succeeded;
 }  
--- Source/TestingGroundsFPS/Public/AI/ChooseNextWaitPoint.h
+++ Source/TestingGroundsFPS/Public/AI/ChooseNextWaitPoint.h
@@ -18,5 +18,10 @@ public:
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+protected:
 
 };
